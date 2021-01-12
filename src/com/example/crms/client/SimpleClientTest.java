d6ba97640d982200e4e39bfd9493ed8179b466ab
kristinau09 <kristinau09@gmail.com>
2021-01-12 14:07:43 -0600

    convert java proj to spring proj Implemented third service class CallHandlingService.java which manages the calls and injected two dependencies through constructor.

--- src/com/example/crms/client/SimpleClientTest.java
+++ src/com/example/crms/client/SimpleClientTest.java
@@ -1,22 +1,46 @@
 package com.example.crms.client;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.example.crms.domain.Action;
+import com.example.crms.domain.Call;
 import com.example.crms.domain.Customer;
+import com.example.crms.services.call.CallHandlingService;
 import com.example.crms.services.customers.CustomerManagementService;
+import com.example.crms.services.customers.CustomerNotFoundException;
+import com.example.crms.services.diary.DiaryManagementService;
 
 public class SimpleClientTest {
 
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext container= new ClassPathXmlApplicationContext("application.xml");
 		
-		CustomerManagementService customerService = container.getBean("customerService", CustomerManagementService.class);
-		//store the result in list
-		List<Customer> allCustomers = customerService.getAllCustomers();
-		for(Customer next: allCustomers) {
-			System.out.println(next);
+		CallHandlingService callService = container.getBean(CallHandlingService.class);
+		DiaryManagementService diaryService = container.getBean(DiaryManagementService.class);
+		Call newCall = new Call("Larry will call from google");
+		Action action1 = new Action("Call by Larry to call him back", new GregorianCalendar(2020,12,2),"abc");
+		Action action2 = new Action("Check our sales department", new GregorianCalendar(2020,12,2),"abc");
+		//create a list of actions where we are passing these two actions
+		List<Action> actions = new ArrayList<Action>();
+		actions.add(action1);
+		actions.add(action2);
+		
+		//if customer id is wrong
+		try {
+			callService.recordCall("101", newCall, actions);
+		}catch(CustomerNotFoundException e) {
+			System.out.println("That customer does not exist");
+		}
+		
+		System.out.println("Here are the outstanding actions: ");
+		Collection<Action> incompleteActions = diaryService.getAllIncompleteActions("abc");
+		for(Action nextAction: incompleteActions) {
+			System.out.println(nextAction);
 		}
 		
 		container.close();
---
+++ src/com/example/crms/services/call/CallHandlingServiceImpl.java
@@ -0,0 +1,39 @@
+package com.example.crms.services.call;
+
+import java.util.Collection;
+
+import com.example.crms.domain.Action;
+import com.example.crms.domain.Call;
+import com.example.crms.services.customers.CustomerManagementService;
+import com.example.crms.services.customers.CustomerNotFoundException;
+import com.example.crms.services.diary.DiaryManagementService;
+
+public class CallHandlingServiceImpl implements CallHandlingService {
+	
+	//dependencies
+	private CustomerManagementService customerService;
+    private DiaryManagementService diaryService;
+    
+    //injecting dependencies through constructor
+    public CallHandlingServiceImpl(CustomerManagementService customerService, DiaryManagementService diaryService) {
+    	this.customerService=customerService;
+    	this.diaryService=diaryService;
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public void recordCall(String customerId, Call newCall, Collection<Action> actions)
+			throws CustomerNotFoundException {
+		//2 steps: 1. call the customer service to record the call
+		customerService.recordCall(customerId, newCall);
+		
+		//2.call the diary service to record the actions
+		//recordAction() takes singular action and since we have been passed a collection of actions, we will loop through
+		for(Action nextAction:actions) {
+			diaryService.recordAction(nextAction);
+		}
+		
+		
+
+	}
+
+}
